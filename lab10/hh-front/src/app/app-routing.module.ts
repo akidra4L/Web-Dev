01de4edb164d46519d690ea346af74344e9cc631
Alikhan <jonasofan.gubaev@gmail.com>
2023-04-15 18:20:44 +0600

    FINISHED

--- lab10/hh-front/src/app/app-routing.module.ts
+++ lab10/hh-front/src/app/app-routing.module.ts
@@ -1,7 +1,21 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+import { CompanyListComponent } from './pages/company-list/company-list.component';
+import { CompanyComponent } from './pages/company/company.component';
+import { HomePageComponent } from './pages/home-page/home-page.component';
+import { VacancyListComponent } from './pages/vacancy-list/vacancy-list.component';
+import { VacancyComponent } from './pages/vacancy/vacancy.component';
+
+const routes: Routes = [
+  { path: 'home', component: HomePageComponent },
+  { path: 'companies', component: CompanyListComponent },
+  { path: 'companies/:id', component: CompanyComponent },
+  { path: 'companies/:id/vacancies', component: VacancyListComponent },
+  { path: 'vacancies', component: VacancyListComponent },
+  { path: 'vacancies/:id', component: VacancyComponent },
+  { path: '', redirectTo: 'home', pathMatch: 'full' },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- lab10/hh_back/hh_back/settings.py
+++ lab10/hh_back/hh_back/settings.py
@@ -47,16 +47,17 @@ INSTALLED_APPS = [
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
+    "corsheaders.middleware.CorsMiddleware",
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
-    'corsheaders.middleware.CorsMiddleware',
-    'django.middleware.common.CommonMiddleware',
 ]
 
-CORS_ORIGIN_ALLOW_ALL = True
+CORS_ALLOWED_ORIGINS = [
+    'http://localhost:4200',
+]
 
 ROOT_URLCONF = 'hh_back.urls'
 
